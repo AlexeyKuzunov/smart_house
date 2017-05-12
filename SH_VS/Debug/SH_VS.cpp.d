820343aa501ef2aa2a945a159ae89108db927cfc
Алексей <@ALEX-HOME>
2017-05-12 19:21:38 +0300

    01.01.17

---
+++ SH_VS/Debug/SH_VS.cpp.d
@@ -0,0 +1,21 @@
+d:\tmp\VMBuilds\SH_VS\diecimila_atmega328\Debug\SH_VS.cpp.o: \
+ d:\tmp\VMBuilds\SH_VS\diecimila_atmega328\Debug\SH_VS.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ D:\Hobby\Arduino\libraries\OneWire/OneWire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Hobby\Arduino\libraries\RF24/RF24_config.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ D:\Hobby\Arduino\libraries\RF24/RF24.h \
+ D:\Hobby\Arduino\libraries\RF24/RF24_config.h \
+ d:\tmp\VMBuilds\SH_VS\diecimila_atmega328\Debug\ASLibrary.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Community\Common7\IDE\Extensions\02oghilb.30b\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h

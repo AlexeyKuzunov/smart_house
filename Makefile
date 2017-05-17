d158da61d97075c04e270263f8b9aa91ab2f3bfd
AlexeyKuzunov <a.kuzunov@gmail.com>
2017-05-17 23:24:06 +0300

    17.05.2007_Linux

--- Makefile
+++ Makefile
@@ -1,36 +1,10 @@
-PREFIX=/usr/local/
+LIBNAME=rf24-sunxi
+LIBDIR=./lib/
 
-LIB=librf24-sun7i
-LIBNAME=$(LIB).so.1.0
-OBJDIR=obj/
-LIBDIR=lib/
+all: server
 
-CCFLAGS=-Ofast -mfpu=vfp -mfloat-abi=hard
-
-all:rf24_sh
-
-librf24-sun7i:${OBJDIR}spi.o ${OBJDIR}gpio.o ${OBJDIR}RF24.o
-	g++ -shared -Wl,-soname,$@.so.1 ${CCFLAGS} -o ${LIBDIR}${LIBNAME} $^
-
-${OBJDIR}spi.o:src/spi.cpp
-	g++ -Wall -fPIC ${CCFLAGS} -c $^ -o $@
-
-${OBJDIR}gpio.o:src/gpio.cpp
-	g++ -Wall -fPIC ${CCFLAGS} -c $^ -o $@
-
-${OBJDIR}RF24.o:src/RF24.cpp
-	g++ -Wall -fPIC ${CCFLAGS} -c $^ -o $@
-
-rf24_sh:
-	g++ -Isrc/ src/*.cpp *.cpp -o rf24_sh
+server:
+	g++ main.cpp -g -I./src/ -L${LIBDIR} -l${LIBNAME} -o server -Wno-write-strings
 
 clean:
-	rm -rf ${OBJDIR}*.o ${PREFIX} ${LIBDIR} ${LIBNAME}
-
-install:
-	@echo "[Install]"
-	@if ( test ! -d ${PREFIX}lib ) ; then mkdir -p ${PREFIX}lib ; fi
-	@install -m 0755 ${LIBDIR}${LIBNAME} ${PREFIX}${LIBDIR}
-	@ln -sf ${LIBDIR}${LIBNAME} ${PREFIX}${LIBDIR}${LIB}.so.1
-	@ln -sf ${PREFIX}${LIBDIR}${LIB}.so.1 ${PREFIX}${LIBDIR}${LIB}.so
-	@ldconfig
+	rm -f server
--- main.cpp
+++ main.cpp
@@ -1,113 +1,40 @@
-/*
-формат запуска
-
-список параметров для подачи команды датчику
-имя файла -m модуль -s датчик -c команда датчику
-
-чтение показаний с одного датчика
-имя файла -m модуль -s датчик
-
-чтение показаний со всех датчиков на модуле 
-имя файла -m модуль
-
-отображение всех подключенных модулей с датчиками
-имя файла
-
-*/
-
 #include "gpio.h"
 #include "RF24.h"
-#include <getopt.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "ASLibrary.h"
 
-/* Имя программы. */
-const char* program_name;
+const uint64_t pipes[2] = {0xF0F0F0F0E1LL,0xF0F0F0F0E2LL};
 
-/* Вывод информации об использовании программы в поток STREAM
-(обычно stdout или stderr) и завершение работы с выдачей кода
-EXIT_CODE. Возврат в функцию main() не происходит */
-void print_usage(FILE* stream, int exit_code) {
-	fprintf(stream, "Usage: %s options [ inputfile ... ]\n",
-	program_name);
-	fprintf(stream,
-	" -h --help Display this usage information.\n"
-	" -m --module указание модуля.\n"
-	" -s --sensor указание датчика.\n"
-	" -с --command указание команды.\n");
-	exit(exit_code);
+RF24 radio(SUNXI_GPB(13), "/dev/spidev0.0");
+
+
+void init_nrf(void)
+{
+	// установка прерываний пока не используем
+//	gpio_export(int_gpio_num);
+//	gpio_set_edge(GPIO_STR, "rising", "1"); //отслеживаем переход из неактивного состояния  активное
+
+	radio.begin(); //старт работы
+	// enable dynamic payloads
+	radio.enableAckPayload(); //Разрешение отправки нетипового ответа передатчику
+	radio.enableDynamicPayloads();
+	radio.setAutoAck(1); //Установка режима подтверждения приема
+	radio.setRetries(15, 15); //Установка интервала и количества попыток "дозвона" до приемника
+	radio.setDataRate(RF24_2MBPS); //Установка минимальной скорости
+	radio.setPALevel(RF24_PA_HIGH); //Установка максимальной мощности
+	radio.setChannel(50); //Установка канала вещания
+	radio.setCRCLength(RF24_CRC_16);
+	// Open pipes to other nodes for communication
+	// Open pipe for reading
+	radio.openReadingPipe(0, pipes[0]);
+	radio.openReadingPipe(1, pipes[1]);
+	mkdir(DIR_PIPES, 0775);
+	// Start listening
+	radio.startListening();
+	// Dump the configuration of the rf unit for debugging
+	radio.printDetails();
 }
 
-/* Точка входа в основную программу, параметр ARGC содержит размер
-списка аргументов; параметр ARGV -- это массив указателей
-на аргументы. */
-int main(int argc, char* argv[]) {
-	int next_option;
-
-	/* Строка с описанием возможных коротких опций. */
-	const char* const short_options = "hm:s:c:";
-
-	/* Массив с описанием возможных длинных опций. */
-	const struct option long_options[] = {
-		{ "help", 0, NULL, 'h' },
-		{ "module", 1, NULL, 'm' },
-		{ "sensor", 1, NULL, 's' },
-		{ "command", 1, NULL, 'c' },
-		{ NULL, 0, NULL, 0 } /* Требуется в конце массива. */
-	};
-
-	/* Имя файла, в который записываются результаты работы
-	программы, или NULL, если вывод направляется в поток
-	stdout. */
-	const char* output_filename = NULL;
-
-	/* Следует ли выводить развернутые сообщения. */
-	int verbose = 0;
-
-	/* Запоминаем имя программы, которое будет включаться
-	в сообщения. Оно хранится в элементе argv[0] */
-	program_name = argv[0];
-
-do {
-	next_option = getopt_long(argc, argv, short_options, long_options, NULL);
-	switch(next_option) {
-		case 'h': /* -h или --help */
-			/* Пользователь запросил информацию об использовании
-			программы, нужно вывести ее в поток stdout и завершить
-			работу с выдачей кода 0 (нормальное завершение). */
-			print_usage(stdout, 0);
-			break;
-		case 'm': /* */
-			printf ("параметр m со значением `%s'\n", optarg);
-			break;
-		case 's': /* */
-			printf ("параметр s со значением `%s'\n", optarg);
-			break;
-		case 'c': /* */
-			printf ("параметр c со значением `%s'\n", optarg);
-			break;
-		
-		case '?': /* Пользователь ввел неверную опцию. */
-			/* Записываем информацию об использовании программы в поток
-			stderr и завершаем работу с выдачей кода 1
-			(аварийное завершение). */
-			print_usage(stderr, 1);
-		case -1: /* Опций больше нет. */
-			break;
-		default: /* Какой-то непредвиденный результат. */
-	abort();
-	}
-}
-while (next_option != -1);
-
-/* Обработка опций завершена, переменная OPTIND указывает на
-первый аргумент, не являющийся опцией. В демонстрационных
-целях отображаем эти аргументы, если задан режим VERBOSE. */
-if (verbose) {
-	int i;
-	for (i = optind; i < argc; ++i)
-	printf("Argument: %s\n", argv[i]);
-}
-/* Далее идет основное тело программы... */
-return 0;
+int main(int argc, char** argv)
+{
+	init_nrf();
 }
